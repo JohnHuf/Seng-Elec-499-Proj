cdc836d11141605cb8af6e01d43a83404247289e
JohnHuf <jfhufnagel@gmail.com>
2016-07-02 11:32:13 -0700

    Added working system with master loop and fastwire

---
+++ working_sensor_system/blueduino_499_proj_init.h
@@ -0,0 +1,45 @@
+#ifndef _BLUEDUINO_499_PROJ_INIT_H
+#define _BLUEDUINO_499_PROJ_INIT_H
+
+#include "high_g_accel.h"
+#include "MPU6050.h"
+
+#include <SPI.h>
+#include <Wire.h>
+
+#define USB_BAUD_RATE 9600
+#define LOW_G_1KHz    7
+
+MPU6050 _lowG_Gyro;
+
+/** 
+ *  Initialization function
+**/
+void blueduino_init(){
+	//SPI initialization
+	pinMode(SPI_CS, OUTPUT);
+	digitalWrite(SPI_CS, HIGH);
+	SPI.beginTransaction(SPISettings(HIGH_G_ACCEL_MAX_SPI_CLK, HIGH_G_ACCEL_BIT_ORDER, HIGH_G_ACCEL_SPI_MODE));
+	SPI.begin();
+	
+	//High G accelerometer init
+	high_g_init();
+
+  /**
+   * Low G/Gyro Init
+   * Using MPU6050_test.ino as reference file for initalization procedure
+   **/
+  //Wire.begin();
+  _lowG_Gyro.initialize();
+  //set sample rate to 1 KHz (Same as High G)
+  _lowG_Gyro.setRate(LOW_G_1KHz);
+  //Set G range to 16
+  _lowG_Gyro.setFullScaleAccelRange(MPU6050_ACCEL_FS_16);
+  //Leave Gyro for now? 250
+	
+	//USB serial 
+	Serial.begin(USB_BAUD_RATE);
+}
+
+
+#endif
